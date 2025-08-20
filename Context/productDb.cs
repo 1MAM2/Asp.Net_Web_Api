2703d550a5e108000044ed4fd6ab95a2fb5557f6
Mahmut <139115718+1MAM2@users.noreply.github.com>
2025-08-21 00:55:10 +0300

    railway kullanmaya başladık

--- Context/productDb.cs
+++ Context/productDb.cs
@@ -5,11 +5,21 @@ namespace productApi.Context
 
     public class productDb : DbContext
     {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("products"); // küçük harf
+            modelBuilder.Entity<Category>().ToTable("categories"); // küçük harf
+            modelBuilder.Entity<ProductImage>().ToTable("productimages"); // küçük harf
+
+        }
+
         public productDb(DbContextOptions<productDb> options) : base(options) { }
 
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Category> Categories => Set<Category>();
         public DbSet<ProductImage> ProductImages => Set<ProductImage>();
+
+
 
     }
 }
