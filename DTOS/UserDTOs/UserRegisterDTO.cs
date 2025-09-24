417a5e47ffa87ee2003f9b620b30fc573395c3de
Mahmut <139115718+1MAM2@users.noreply.github.com>
2025-09-24 22:18:57 +0300

    user update

---
+++ DTOS/UserDTOs/UserRegisterDTO.cs
@@ -0,0 +1,11 @@
+
+
+
+public class UserRegisterDTO
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Role { get; set; } = "Customer";
+    public string Email { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+}
