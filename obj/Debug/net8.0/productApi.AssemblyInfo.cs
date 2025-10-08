cb573735a0d79c0e21090da4bd546ba1b03b2392
Mahmut <139115718+1MAM2@users.noreply.github.com>
2025-10-08 23:51:23 +0300

    payment kısmı canlı

--- obj/Debug/net8.0/productApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/productApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("productApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c629d070a8991ca2b1f9e0957ad8e90fe0bc0af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec62f5a8d8c86d8bc4615122d492bea7365b890c")]
 [assembly: System.Reflection.AssemblyProductAttribute("productApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("productApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
